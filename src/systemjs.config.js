a7b80c870e3dadaee581887cbe87d662740f90df
skipperhoa <thanhhoacth2013@gmail.com>
2017-08-26 16:47:47 +0700

    ckeditor-angular-v2

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -7,12 +7,13 @@
     paths: {
       // paths serve as alias
       'npm:': 'node_modules/'
+      
     },
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
       'app': 'app',
-
+      'ng2-ckeditor': 'npm:ng2-ckeditor',
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -37,9 +38,14 @@
           }
         }
       },
+      "ng2-ckeditor": {
+        "main": "lib/index.js",
+        "defaultExtension": "js",
+      },
       rxjs: {
         defaultExtension: 'js'
       }
     }
+    
   });
 })(this);
